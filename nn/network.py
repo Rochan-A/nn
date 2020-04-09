048e238635273414971b9b3329fbc201e25fc6e7
ZakariaZeta <62648170+ZakariaZeta@users.noreply.github.com>
2020-04-09 16:38:45 +0530

    Update network.py

--- nn/network.py
+++ nn/network.py
@@ -1,214 +1,10 @@
 import torch
 from torch import nn
-from .utils import tanh, d_tanh
-
-import numpy as np
-
-from keras.models import Sequential
-from keras import regularizers
-from keras.layers import Dense, Dropout
-from keras.initializers import lecun_normal, Constant
 
 torch.set_printoptions(precision=4)
 torch.set_default_dtype(torch.float32)
 torch.Generator().manual_seed(2147483647)
 print("Initial Seed: ", torch.Generator().initial_seed())
-
-class ff_network(Sequential):
-    """Fully Connected Neural Network"""
-
-    def __init__(self,
-                 learning_rate=1e-3,
-                 momentum=0.9,
-                 bias=None,
-                 batch_size=1
-                 ):
-        """Intialize weights
-
-        Args:
-            learning_rate (Default=1e-5): Learning rate
-            momentum (Default=0.9): Momentum factor
-            bias (Defualt=None): Use bais value or not
-            batch_size (Default=1): Batch Size
-        """
-        super().__init__()
-
-        # Parameters
-        self.lr = learning_rate
-        self.momentum = momentum
-        self.bias = bias
-        self.batch_size = batch_size
-        self.num_layers = 0
-
-        # Auxilary matrix for storing momentum calculation
-        self.w_delta = []
-        self.b_delta = []
-
-    def add_linear(self,
-                   input_dim,
-                   output_dim,
-                   INPUT=False
-                   ):
-        """Add layer to the network
-
-        Args:
-            input_dim: Input dimension
-            output_dim: Output dimension
-        """
-        if INPUT:
-            self.add(Dense(output_dim=output_dim,
-                           use_bias=self.bias,
-                           kernel_initializer=lecun_normal(seed=None),
-                           bias_initializer=Constant(0.1),
-                           activation='tanh',
-                           input_dim=input_dim)
-                     )
-        else:
-            self.add(Dense(output_dim=output_dim,
-                           use_bias=self.bias,
-                           kernel_initializer=lecun_normal(seed=None),
-                           bias_initializer=Constant(0.1),
-                           activation='tanh')
-                     )
-
-        # Weight delta for momentum calculation
-        w_d = np.zeros([output_dim, input_dim])
-        if self.bias:
-            b_d = np.zeros(self.layers[self.num_layers].get_weights()[1].shape)
-
-        self.w_delta.append(w_d)
-        self.b_delta.append(b_d)
-        self.num_layers += 1
-
-    def forward(self, x):
-        """Forward pass on Neural Network
-
-        Args:
-            x: Input matrix
-
-        Returns:
-            output: Output matrix
-        """
-        x = np.array(x, ndmin=2).T
-
-        self.v = []
-        self.o = []
-
-        # Iterate over every layer and forward pass, store v and o
-        # values.
-        for layer_idx in range(self.num_layers):
-            weight = self.layers[layer_idx].get_weights()[0]
-            v = np.dot(weight.T, x)
-            if self.bias:
-                bias = self.layers[layer_idx].get_weights()[1]
-                bias = np.expand_dims(bias, axis=1)
-                v += bias
-            x = tanh(v)
-
-            self.v.append(v)
-            self.o.append(x)
-
-        return x
-
-    def backwards(self, inp, expected):
-        """Forward pass on Neural Network
-
-        Args:
-            inp: Input matrix
-            expected: Expected value matrix
-        """
-        inp = np.array(inp, ndmin=2).T
-
-        # Compute error at last layer
-        self.o_error = np.mean(expected.T - self.o[self.num_layers - 1],
-                               axis=1, keepdims=True)
-
-        # Note: Refer backpropogation slides for full details on
-        #       backprop equation.
-        # TODO: Backpropogation through multiple layers in a loop.
-
-        # Pass output loss through derivative of activation function.
-        # In this case, Tanh.
-        self.llayer_error = self.o_error * \
-            np.mean(d_tanh(self.v[self.num_layers - 1]),
-                    axis=1, keepdims=True)
-
-        # Compute delta of weights in layer. Scale delta by a factor of
-        # learning rate.
-        delta_llast_layer = self.lr * \
-            np.dot(self.llayer_error,
-                   np.mean(self.o[self.num_layers - 2],
-                           axis=1, keepdims=True).T)
-
-        # Add delta and momentum factor for improved stability to
-        # weight matrix.
-        weight = self.layers[self.num_layers - 1].get_weights()[0]
-        if self.bias:
-            bias = self.layers[self.num_layers - 1].get_weights()[1]
-            bias_delta = self.lr * self.llayer_error.reshape(-1)
-            updatedWeights = [weight +
-                              delta_llast_layer.T +
-                              self.momentum *
-                              self.w_delta[self.num_layers -
-                                           1].T, bias +
-                              bias_delta +
-                              self.momentum *
-                              self.b_delta[self.num_layers -
-                                           1]]
-        else:
-            updatedWeights = [weight +
-                              (1 - self.momentum) * delta_llast_layer.T +
-                              self.momentum * self.w_delta[self.num_layers - 1].T]
-
-        self.layers[self.num_layers - 1].set_weights(updatedWeights)
-        # Update momentum factor
-        self.w_delta[self.num_layers - 1] = delta_llast_layer
-        self.b_delta[self.num_layers - 1] = bias_delta
-
-        # Note: The above steps are repeated for the two other weight
-        # matrices.
-        for layer_idx in range(self.num_layers - 2, -1, -1):
-            if self.bias:
-                self.llayer_error = np.dot(self.layers[layer_idx + 1].get_weights()[0],
-                                           self.llayer_error) * np.mean(d_tanh(self.v[layer_idx]), axis=1, keepdims=True)
-
-            if layer_idx == 0:
-                delta_llast_layer = self.lr * \
-                    np.dot(self.llayer_error, np.mean(
-                        inp, axis=1, keepdims=True).T)
-            else:
-                delta_llast_layer = self.lr * \
-                    np.dot(self.llayer_error, np.mean(
-                        self.o[layer_idx - 1], axis=1, keepdims=True).T)
-
-            weight = self.layers[layer_idx].get_weights()[0]
-            if self.bias:
-                bias = self.layers[layer_idx].get_weights()[1]
-                bias_delta = self.lr * self.llayer_error.reshape(-1)
-                updatedWeights = [
-                    weight +
-                    delta_llast_layer.T +
-                    self.momentum *
-                    self.w_delta[layer_idx].T,
-                    bias +
-                    bias_delta +
-                    self.momentum *
-                    self.b_delta[layer_idx]]
-            else:
-                updatedWeights = [weight +
-                                  (1 -
-                                   self.momentum) *
-                                  delta_llast_layer.T +
-                                  self.momentum *
-                                  self.w_delta[layer_idx].T]
-
-            self.layers[layer_idx].set_weights(updatedWeights)
-            self.w_delta[layer_idx] = delta_llast_layer
-            self.b_delta[layer_idx] = bias_delta
-
-        del self.o
-        del self.v
-
 
 def init_weights_agent(m):
     """Initialize the weights and bias values of network layer
@@ -221,7 +17,7 @@ def init_weights_agent(m):
         try:
             nn.init.xavier_uniform_(m.bias)
         except BaseException:
-            a = 0
+            None
 
 
 @torch.jit.script
@@ -242,10 +38,7 @@ class pytorch_network(nn.Module):
     """Fully Connected Neural Network"""
 
     def __init__(self,
-                 learning_rate=1e-3,
-                 momentum=0.9,
                  bias=None,
-                 batch_size=1,
                  pop_size=None,
                  k_val=None,
                  cross_prob=None
@@ -264,10 +57,8 @@ class pytorch_network(nn.Module):
         super().__init__()
 
         # Parameters
-        self.lr = learning_rate
-        self.momentum = momentum
+
         self.bias = bias
-        self.batch_size = batch_size
         self.num_layers = 0
 
         # DE parameters
@@ -284,9 +75,6 @@ class pytorch_network(nn.Module):
         # Network layers
         self.layers = nn.Sequential()
 
-        # Auxilary matrix for storing momentum calculation
-        self.w_delta = []
-        self.b_delta = []
 
     def add_linear(self,
                    input_dim,
@@ -304,16 +92,15 @@ class pytorch_network(nn.Module):
         self.layers.apply(init_weights_agent)
 
         # Weight delta for momentum calculation
-        w_d = torch.zeros([output_dim, input_dim])
         if self.bias:
-            b_d = torch.zeros(self.layers[self.num_layers].bias.shape)
             b_shape = self.layers[self.num_layers].bias.numel()
+        else :
+            b_shape = 0
 
         # Initialize candidates
-        if self.pop_init:
+        if INPUT :
             self.population = nn.init.xavier_uniform_(torch.empty(
                 self.pop_size, input_dim * output_dim + b_shape))
-            self.pop_init = False
         else:
             self.population = torch.cat(
                 (self.population,
@@ -325,8 +112,6 @@ class pytorch_network(nn.Module):
                          b_shape))),
                 dim=1)
 
-        self.w_delta.append(w_d)
-        self.b_delta.append(b_d)
         self.num_layers += 1
 
     def _set_weights_to_layers(self, candidate):
@@ -342,19 +127,22 @@ class pytorch_network(nn.Module):
             # Get layer dimensions
             w_shape = torch.tensor(self.layers[layer_idx].weight.shape)
             w_numel = torch.prod(w_shape).item()
-            b_shape = torch.tensor(self.layers[layer_idx].bias.shape)
-            b_numel = torch.prod(b_shape).item()
+            if self.bias :
+                b_shape = torch.tensor(self.layers[layer_idx].bias.shape)
+                b_numel = torch.prod(b_shape).item()
 
             # Decode the candidate and get weight, bias matrices
             weight = candidate[last_idx:last_idx +
                                w_numel].reshape(tuple(w_shape))
             last_idx += w_numel
-            bias = candidate[last_idx:last_idx + b_numel].reshape(b_shape)
-            last_idx += b_numel
+            if self.bias :
+                bias = candidate[last_idx:last_idx + b_numel].reshape(b_shape)
+                last_idx += b_numel
 
             # Set layer weight, bias
             self.layers[layer_idx].weight = torch.nn.Parameter(weight)
-            self.layers[layer_idx].bias = torch.nn.Parameter(bias)
+            if self.bias :
+                self.layers[layer_idx].bias = torch.nn.Parameter(bias)
 
     def _mutant(self, idx, F):
         """Generate Mutant vector and perform Crossover
@@ -385,27 +173,6 @@ class pytorch_network(nn.Module):
             mutant,
             self.population[idx])
 
-    def forward_de(self, x):
-        """Forward pass on Neural Network candidates
-
-        Args:
-            x: Input matrix
-
-        Returns:
-            output: Output matrix,
-                    resulting shape (pop_size, batch_size, output_size)
-        """
-        x = torch.tensor(x)
-        y = []
-
-        # Iterate over candidates
-        for idx, candidate in enumerate(self.population):
-            self._set_weights_to_layers(candidate)
-            xx = self.forward_bp(x)
-            y.append(xx.numpy())
-        self.y = torch.tensor(y)
-
-        return y
 
     def backwards_de(self, input_, expected):
         """Backwards pass on Neural Network using Differential Evolution
@@ -424,7 +191,8 @@ class pytorch_network(nn.Module):
             # Iterate over candidates
             for idx, _ in enumerate(self.population):
                 # Compute loss of candidate
-                loss = torch.mean(_objective(self.y[idx], expected))
+                vec_output = self.predict(input_)
+                loss = torch.mean(_objective(vec_output, expected))
 
                 # Perform DE, if the generated weights perform worse, recompute
                 # till it performs better than existing candidate
@@ -433,8 +201,8 @@ class pytorch_network(nn.Module):
                     trial = self._mutant(idx, F)
 
                     self._set_weights_to_layers(trial)
-                    trial_output = self.predict(input_)
-                    trial_loss = torch.mean(_objective(trial_output, expected))
+                    vec_output = self.predict(input_)
+                    trial_loss = torch.mean(_objective(vec_output, expected))
                     #print(trial_loss, loss)
 
                     if trial_loss <= loss:
@@ -448,112 +216,6 @@ class pytorch_network(nn.Module):
 
         # Save the candidates loss history
         self.candidate_loss = torch.cat((self.candidate_loss, gen_loss), dim=1)
-
-    def forward_bp(self, x):
-        """Forward pass on Neural Network
-        Args:
-            x: Input matrix
-        Returns:
-            output: Output matrix
-        """
-        x = torch.tensor(x)
-
-        self.v = []
-        self.o = []
-
-        with torch.no_grad():
-            # Iterate over every layer and forward pass, store v and o
-            # values.
-            for layer_idx in range(self.num_layers):
-                weight = self.layers[layer_idx].weight
-                v = torch.matmul(x, weight.T)
-                if self.bias:
-                    bias = self.layers[layer_idx].bias
-                    v += bias.T
-                x = torch.tanh(v)
-
-                self.v.append(v)
-                self.o.append(x)
-
-        return x
-
-    def backwards_bp(self, inp, expected):
-        """Backwards pass on Neural Network using Backpropogation
-
-        Args:
-            inp: Input matrix
-            expected: Expected value matrix
-        """
-        inp = torch.tensor(inp)
-        expected = torch.tensor(expected)
-
-        with torch.no_grad():
-
-            # Compute error at last layer
-            self.o_error = torch.mean(expected - self.o[self.num_layers - 1],
-                                      dim=0, keepdims=True)
-
-            # Note: Refer backpropogation slides for full details on
-            #       backprop equation.
-            # TODO: Backpropogation through multiple layers in a loop.
-
-            # Pass output loss through derivative of activation function.
-            # In this case, Tanh.
-            self.llayer_error = self.o_error * \
-                torch.mean(d_tanh(self.v[self.num_layers - 1]),
-                           dim=0, keepdims=True)
-
-            # Compute delta of weights in layer. Scale delta by a factor of
-            # learning rate.
-            delta_llast_layer = self.lr * \
-                torch.matmul(self.llayer_error,
-                             torch.mean(self.o[self.num_layers - 2],
-                                        dim=0, keepdims=True))
-
-            # Add delta and momentum factor for improved stability to
-            # weight matrix.
-            self.layers[self.num_layers - 1].weight += \
-                1 * delta_llast_layer + \
-                self.momentum * self.w_delta[self.num_layers - 1]
-            if self.bias:
-                self.layers[self.num_layers - 1].bias += \
-                    self.lr * self.llayer_error.reshape(-1) + \
-                    self.momentum * self.b_delta[self.num_layers - 1]
-            # Update momentum factor
-            self.w_delta[self.num_layers - 1] = delta_llast_layer
-            self.b_delta[self.num_layers - 1] = self.lr * \
-                self.llayer_error.reshape(-1)
-
-            # Note: The above steps are repeated for the two other weight
-            # matrices.
-            for layer_idx in range(self.num_layers - 2, -1, -1):
-                if self.bias:
-                    self.llayer_error = torch.matmul(self.llayer_error,
-                                                     self.layers[layer_idx + 1].weight) * \
-                        torch.mean(d_tanh(self.v[layer_idx]),
-                                   dim=0, keepdims=True)
-
-                if layer_idx == 0:
-                    delta_llast_layer = self.lr * \
-                        torch.matmul(self.llayer_error.T,
-                                     torch.mean(inp, dim=0, keepdims=True))
-                else:
-                    delta_llast_layer = self.lr * \
-                        torch.matmul(self.llayer_error.T,
-                                     torch.mean(self.o[layer_idx - 1], dim=0, keepdims=True))
-
-                self.layers[layer_idx].weight += 1 * delta_llast_layer + \
-                    self.momentum * self.w_delta[layer_idx]
-                if self.bias:
-                    self.layers[layer_idx].bias += self.lr * self.llayer_error.reshape(-1) + \
-                        self.momentum * self.b_delta[layer_idx]
-
-                self.w_delta[layer_idx] = delta_llast_layer
-                self.b_delta[layer_idx] = self.lr * \
-                    self.llayer_error.reshape(-1)
-
-            del self.o
-            del self.v
 
     def predict(self, x):
         """Forward pass on Neural Network for prediction
